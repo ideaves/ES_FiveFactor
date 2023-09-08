9eedea541d558f21ecfdd9a14f7067f9b6e685e4
Dave Eaves <142260318+ideaves@users.noreply.github.com>
2023-09-08 07:59:02 -0700

    Cleaned up version with code re-use

---
+++ ES_FiveFactor_CurrentPrediction.py
@@ -0,0 +1,5 @@
+import FuturesHelper;
+import ES_FiveFactor_LearningModelPool;
+
+one_time_predictor : ES_FiveFactor_LearningModelPool = ES_FiveFactor_LearningModelPool.ES_FiveFactor_LearningModelPool()
+one_time_predictor.calculate_current_prediction()
